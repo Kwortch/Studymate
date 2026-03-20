1c8790a0fe0464278a5829c70919552953573d2a
Kwortch <qkjaavenido@tip.edu.ph>
2026-03-20 22:44:43 +0800

    2

--- StudyMate/app/_layout.tsx
+++ StudyMate/app/_layout.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack, useRouter, useSegments } from 'expo-router';
+import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
@@ -12,26 +11,6 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [isReady, setIsReady] = useState(false);
-  const router = useRouter();
-  const segments = useSegments();
-
-  useEffect(() => {
-    // simulate loading (replace with real logic later)
-    setTimeout(() => {
-      setIsReady(true);
-    }, 2000);
-  }, []);
-
-  useEffect(() => {
-    if (isReady && segments[0] !== '(tabs)') {
-      router.replace('/(tabs)'); // go to main app after splash
-    }
-  }, [isReady]);
-
-  if (!isReady) {
-    return null; // keep splash visible (you can render custom UI here)
-  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
